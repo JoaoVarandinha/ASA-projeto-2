90b20f5db0183c535873ddd7abce0c69bedbcebf
João Varanda <joao.m.varanda@tecnico.ulisboa.pt>
2025-12-15 14:11:40 +0000

    Teach cmds fix

--- ASA-projeto2.cpp
+++ ASA-projeto2.cpp
@@ -36,8 +36,8 @@ vector<vector<string>> solve(input_data info) {
 
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     input_data info = readInput();
 
