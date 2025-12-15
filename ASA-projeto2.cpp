e035ad6871ca8dcb175b199db831bb8cfbf059d3
João Varanda <joao.m.varanda@tecnico.ulisboa.pt>
2025-12-15 17:17:52 +0000

    So close

--- ASA-projeto2.cpp
+++ ASA-projeto2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <string>
 
 using namespace std;
 
@@ -12,19 +14,19 @@ typedef struct {
 } input_data;
 
 
-input_data readInput() {
+const input_data readInput() {
     input_data info;
 
     //Read number of intersections, number of trucks, range of truck numbers and number of paths
     cin >> info.num_inter >> info.total_trucks >> info.truck_min >> info.truck_max >> info.num_paths;
 
-    info.paths.reserve(info.num_paths);
+    info.paths.resize(info.num_inter + 1);
 
-    //Read guided paths between intersections
+    //Read the paths between intersections and turn them into a graph
     for (int i = 0; i < info.num_paths; i++) {
-        int a, b;
-        cin >> a >> b;
-        info.paths[a].push_back(b);
+        int origin, destination;
+        cin >> origin >> destination;
+        info.paths[origin].push_back(destination);
     }
 
     return info;
@@ -34,43 +36,113 @@ int calculateTruckToPath(int total_trucks, int total_paths) {
     return 1 + (total_paths % total_trucks);
 }
 
-vector<vector<string>> solve(input_data info) {
-    vector<vector<int>> total_paths(info.num_paths + 1, vector<int>(info.num_paths + 1, 0));
-    vector<vector<string>> truck_paths(info.total_trucks);
-    vector<int> top_order(info.num_inter + 1);
+vector<int> getTopologicalOrder(const input_data info) {
+    vector<int> top_order;
+    vector<int> degrees(info.num_inter + 1, 0);
 
-    for (int i = 1; i < info.num_paths + 1; i++) {
-        if (info.paths[i].size() == 0) continue;
+    for (int i = 1; i <= info.num_inter; i++) {
+        for (int j : info.paths[i])
+            degrees[j]++;
+    }
 
-        for (int j = 1; j < info.num_paths + 1; j++) {
-            if (i == j) continue;
+    queue<int> q;
+    for (int j = 1; j <= info.num_inter; j++) {
+            if (degrees[j] == 0) q.push(j);
+    }
 
+    while (!q.empty()) {
+        int inter = q.front();
+        q.pop();
+        top_order.push_back(inter);
 
-            
+        for (int k : info.paths[inter]) {
+            degrees[k]--;
+            if (degrees[k] == 0) q.push(k);
         }
     }
 
-    int truck_num = calculateTruckToPath(info.total_trucks, total_paths[i][j]);
-    if (info.truck_min <= truck_num <= info.truck_max) {
-        truck_paths[truck_num - info.total_trucks] = to_string(i) + "," + to_string(j);
+    return top_order;
+}
+
+vector<vector<string>> solve(const input_data info) {
+    vector<vector<int>> total_paths(info.num_inter + 1, vector<int>(info.num_inter + 1, 0));
+    vector<vector<string>> truck_routes(info.truck_max - info.truck_min + 1);
+    vector<int> top_order = getTopologicalOrder(info);
+    vector<int> paths_a_To_b(info.num_inter + 1, 0);
+    
+
+    for (int i = 1; i <= info.num_inter; i++) {
+        if (info.paths[i].size() == 0) continue;
+
+        for (int j = 1; j <= info.num_inter; j++) {
+            if (i == j) continue;
+
+
+
+        }
+    }
+
+    if (total_paths[i][j] != 0)    {
+        int truck_num = calculateTruckToPath(info.total_trucks, total_paths[i][j]);
+        if ((info.truck_min <= truck_num) && (truck_num <= info.truck_max)) {
+            truck_routes[truck_num - info.truck_min].push_back(to_string(i) + "," + to_string(j));
+        }
     }
     
 
-    return truck_paths;
+    return truck_routes;
 }
+
+
+vector<vector<string>> solver(const input_data info) {
+    vector<vector<int>> total_paths(info.num_inter + 1, vector<int>(info.num_inter + 1, 0));
+    vector<vector<string>> truck_routes(info.truck_max - info.truck_min + 1);
+    vector<int> top_order = getTopologicalOrder(info);
+    // Para cada origem i, fazemos uma DP iterativa ao longo da ordem topológica
+    vector<int> paths_a_To_b(info.num_inter + 1, 0);
+
+    for (int i = 1; i <= info.num_inter; i++) {
+        // Inicializar DP
+        fill(paths_a_To_b.begin(), paths_a_To_b.end(), 0);
+        paths_a_To_b[i] = 1;  // um caminho trivial que começa em i
+
+        // Propagar contagens em ordem topológica
+        for (int j : top_order) {
+            if (paths_a_To_b[j] == 0) continue;  // nada para propagar
+            for (int k : info.paths[j]) {
+                paths_a_To_b[k] += paths_a_To_b[j];
+            }
+        }
+
+        // Guardar resultados: nº de caminhos de i para t
+        for (int t = 1; t <= info.num_inter; t++) {
+            if (i == t || paths_a_To_b[t] == 0) continue;      // ignorar pares A == B
+
+            total_paths[i][t] = paths_a_To_b[t];
+
+            int truck_num = calculateTruckToPath(info.total_trucks, paths_a_To_b[t]);
+            if (truck_num >= info.truck_min && truck_num <= info.truck_max) {
+                truck_routes[truck_num - info.truck_min].push_back(to_string(i) + "," + to_string(t));
+            }
+        }
+    }
+    return truck_routes;
+}
+
+
 
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    input_data info = readInput();
+    const input_data info = readInput();
 
-    vector<vector<string>> truck_paths = solve(info);    
+    vector<vector<string>> truck_routes = solve(info);    
 
-    for (int i = info.truck_min; i < info.truck_max; i++) {
+    for (int i = info.truck_min; i <= info.truck_max; i++) {
         cout << "C" << i;
-        for (string path : truck_paths[i - info.total_trucks]) {
+        for (string path : truck_routes[i - info.truck_min]) {
             cout << " " << path;
         }
         cout << "\n";
